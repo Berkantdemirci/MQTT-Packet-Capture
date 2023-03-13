813cb96cc4be535c80ffa01eca3c8dd4e01bf003
Berkantdemirci <berkantdemirci1905@gmail.com>
2023-03-13 20:43:46 +0300

    signal handler added

--- src/main.c
+++ src/main.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+
 #include "log.h"
 #include "listener.h"
 #include "mqtt_structure.h"
 
+struct handler_struct *handler;
+
+void stop_process(int signo){
+
+    handler->stop(handler->handle);
+}
+
 int main(){
 
-    struct handler_struct *handler;
+    signal(SIGINT, stop_process);
+    signal(SIGTERM, stop_process);
+    signal(SIGQUIT, stop_process);
+    signal(SIGKILL, stop_process);
+    /* call stop_mqtt_capture func after these signals*/
+
     handler = listener_init();
+
     handler->start(handler->handle);
         
     handler->stop(handler->handle);
